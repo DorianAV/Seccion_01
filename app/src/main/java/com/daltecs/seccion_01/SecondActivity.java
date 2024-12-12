0bf510de7405b1b8c369f1a6ad1a0a59e324ef53
doria <doriancinue26@gmail.com>
2024-12-12 11:16:26 -0600

    V1.1

--- app/src/main/java/com/daltecs/seccion_01/SecondActivity.java
+++ app/src/main/java/com/daltecs/seccion_01/SecondActivity.java
@@ -1,7 +1,10 @@
 package com.daltecs.seccion_01;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,8 @@ public class SecondActivity extends AppCompatActivity {
 
 
     private TextView textView;
+    private Button ButtonNext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,7 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.second_activity);
 
         textView = (TextView) findViewById(R.id.textViewMain);
+        ButtonNext = (Button) findViewById(R.id.buttonGoSharing);
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null && bundle.getString("greeter" )!=null){
@@ -33,6 +39,14 @@ public class SecondActivity extends AppCompatActivity {
         else{
             Toast.makeText(SecondActivity.this,"It is empty!", Toast.LENGTH_LONG ).show();
         }
+
+        ButtonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
--- app/src/main/java/com/daltecs/seccion_01/ThirdActivity.java
+++ app/src/main/java/com/daltecs/seccion_01/ThirdActivity.java
@@ -1,24 +1,99 @@
 package com.daltecs.seccion_01;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    private EditText editTextPhone;
+    private EditText editTextWeb;
+    private ImageButton imageBtnPhone;
+    private ImageButton imageBtnWeb;
+    private ImageButton imageBtnCamera;
+    private final int PHONE_CALL_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_third);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextWeb = findViewById(R.id.editTextWeb);
+        imageBtnPhone = findViewById(R.id.imageButtonPhone);
+        imageBtnWeb = findViewById(R.id.imageButtonWeb);
+        imageBtnCamera = findViewById(R.id.imageButtonCamera);
+
+
+        imageBtnPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = editTextPhone.getText().toString();
+                if (phoneNumber != null) {
+                    //Comprobar la version de android
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                        requestPermissions(new String[]{android.Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
+
+                    } else {
+                        OlderVersion(phoneNumber);
+                    }
+                }
+            }
+
+            private void OlderVersion(String phoneNumber) {
+                Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                if (CheckPermission(android.Manifest.permission.CALL_PHONE)) {
+                    startActivity(intentCall);
+                } else {
+//                     requestPermissions(new String[]{android.Manifest.permission.CALL_PHONE}, 1);
+                    Toast.makeText(ThirdActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                startActivity(intentCall);
+            }
+
+
         });
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PHONE_CALL_CODE:
+                String permission = permissions[0];
+                int result = grantResults[0];
+                if(permission.equals(android.Manifest.permission.CALL_PHONE)){
+                    if (result == PackageManager.PERMISSION_GRANTED){
+                        String phoneNumber = editTextPhone.getText().toString();
+                        Intent intentCall = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+                        startActivity(intentCall);
+                    }else{
+                        Toast.makeText(ThirdActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+    }
+
+    private boolean CheckPermission(String permission) {
+        int result = this.checkCallingOrSelfPermission(permission);
+        return result == PackageManager.PERMISSION_GRANTED;
     }
 }
